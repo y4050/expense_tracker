d4f36e9e508c892894082bc18856ac2dd41a4197
J <j@Jacobs-MacBook-Pro.local>
2021-02-17 14:49:28 -0800

    Category.js: added new post route for new category to be added with cloudinary

--- routes/category.js
+++ routes/category.js
@@ -2,6 +2,10 @@ const express = require("express");
 const db = require("../models");
 const router = express.Router();
 
+const multer = require('multer');
+const cloudinary = require('cloudinary');
+// uploader for images, make a uploads folder, pass through the route as middleware
+const uploads = multer({ dest: './uploads'});
 
 router.get("/", (req, res) => {
     db.category.findAll()
@@ -17,5 +21,23 @@ router.get("/new", (req, res) => {
     })
 })
 
+router.post("/", uploads.single('inputFile'), async(req, res) => {
+    try {
+        const image = await req.file.path;
+        console.log(image);
+        const name = await req.body.name;
+        console.log(name)
+        cloudinary.uploader.upload(image, (result) => {
+            // console.log(result);
+            db.category.create({
+                name: name,
+                img: result.url
+            })
+            res.redirect('/category');
+        });
+    }catch(e) {
+        console.log(e.message);
+    }
+});
 
 module.exports = router;
